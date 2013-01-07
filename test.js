76b4ce949ba373da4d3e2695eae8bc5c33d6e9af
Pete Clark <pclarkgithub@gmail.com>
2013-01-07 01:16:21 +0000

    Testing

--- test.js
+++ test.js
@@ -1,19 +1,21 @@
 var path = require("path"), colors = require("colors"), $f = require("fluid");
 var cb = require("codebricks");
 
+
 setupEnvironment(function(err, env) {
 	if (false) {
-		env.methods.BrickTypeDefinition.getBrick({ id : "httpServer1" }, function(err, brick) {
-			console.log(brick);			
+		env.methods.BrickTypes.getBrick({ id : "httpServer1", deep : true }, function(err, brick) {
 			if (err) { console.error(JSON.stringify(err, null, 2).bold.red); } else {
+				console.log(require("util").inspect(brick, null, null, true));	
 				brick.start({}, function(err) {
 					if (err) { console.error(JSON.stringify(err, null, 2).bold.red); } else {
+						console.log("FINSIHED");
 					}
 				});
 			}
 		});
 	} else {		
-		$f(env.methods.BrickTypeDefinition)
+		$f(env.methods.BrickTypes)
 			({name:"httpServer"}).createBrick("codebricks.noodle.httpServer")
 			({name:"httpServer"}).createBrick("codebricks.noodle.httpServer")
 		.go(function(err, result) {
@@ -22,14 +24,19 @@ setupEnvironment(function(err, env) {
 				var httpServer2 = result.httpServer[1];
 
 				httpServer1.__.id = "httpServer1";
-				httpServer1.port = 9123;
 				
 				httpServer2.__.id = "httpServer2";
 				httpServer2.port = 9124;
 				
+				httpServer1.handlers.push(httpServer2);
+				httpServer1.something.prop1 = httpServer2;
+				httpServer2.handlers.push(httpServer1);
+				httpServer2.something.deep.deeper.deepProp.myBrick = httpServer1;
+				
+				
 				httpServer1.start({}, function(err) {
 					if (err) { console.error(JSON.stringify(err, null, 2).bold.red); } else {							
-						env.methods.BrickTypeDefinition.saveBrick({ brick : httpServer1 }, function(err) {
+						env.methods.BrickTypes.saveBrick({ brick : httpServer1, deep : true }, function(err) {
 							if (err) { console.error(JSON.stringify(err, null, 2).bold.red); } else {
 								console.log("BRICK SAVED");
 							}
@@ -37,25 +44,7 @@ setupEnvironment(function(err, env) {
 					}
 				});
 			}
-		});
-		/*
-		env.methods.BrickTypeDefinition.createBrick("codebricks.noodle.httpServer", function(err, serverBrick) {
-			if (err) { console.error(JSON.stringify(err, null, 2).bold.red); } else {
-				serverBrick.__.id = "httpServer1";
-				serverBrick.port = 9123;
-				serverBrick.start({}, function(err) {
-					if (err) { console.error(JSON.stringify(err, null, 2).bold.red); } else {							
-						env.methods.BrickTypeDefinition.saveBrick({ brick : serverBrick }, function(err) {
-							if (err) { console.error(JSON.stringify(err, null, 2).bold.red); } else {
-								console.log("BRICK SAVED");
-							}
-						});
-					}
-				});
-			}
-		});
-		*/
-		
+		});		
 	}
 });
 
@@ -66,10 +55,10 @@ function setupEnvironment(callback) {
 		if (err) { console.error(JSON.stringify(err, null, 2).bold.red); } else {		
 			console.log("TESTING...");
 
-			env.methods.BrickTypeDefinition.createBrick("codebricks.brickRepositories.fileSystem", function(err, repoBrick) {
+			env.methods.BrickTypes.createBrick("codebricks.brickRepositories.fileSystem", function(err, repoBrick) {
 				if (err) { console.error(JSON.stringify(err, null, 2).bold.red); } else {
 					repoBrick.path = path.resolve(__dirname, "testBricks");
-					env.methods.BrickTypeDefinition.registerRepository(repoBrick, function(err) {
+					env.methods.BrickTypes.registerRepository(repoBrick, function(err) {
 						if (err) { console.error(JSON.stringify(err, null, 2).bold.red); } else {
 							callback(null, env);
 						}
--- testBricks/httpServer1.js
+++ testBricks/httpServer1.js
@@ -1,5 +1,28 @@
 {
-  "port": 9123,
+  "something": {
+    "prop1": {
+      "__": {
+        "id": "httpServer2",
+        "brickTypeId": "codebricks.noodle.httpServer"
+      }
+    },
+    "prop2": {
+      "hash1": 1,
+      "hash2": true
+    },
+    "deep": {
+      "deeper": {}
+    }
+  },
+  "port": 8123,
+  "handlers": [
+    {
+      "__": {
+        "id": "httpServer2",
+        "brickTypeId": "codebricks.noodle.httpServer"
+      }
+    }
+  ],
   "__": {
     "id": "httpServer1",
     "brickTypeId": "codebricks.noodle.httpServer"
